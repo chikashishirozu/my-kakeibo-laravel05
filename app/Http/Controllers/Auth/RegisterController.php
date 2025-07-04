5399ef4eb97c5b76b43ccb56bccdf52319ff7553
CHIKASHI SHIROZU <102590095+chikashishirozu@users.noreply.github.com>
2025-07-05 04:44:36 +0900

    commit new file
    commit new file

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,45 @@
+<?php
+
+// app/Http/Controllers/Auth/RegisterController.php
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules;
+
+class RegisterController extends Controller
+{
+    /**
+     * 登録フォームを表示
+     */
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * ユーザー登録処理
+     */
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/');
+    }
+}
+
